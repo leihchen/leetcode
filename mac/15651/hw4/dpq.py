ecb59799da1ca078c456faa1fa2317da919f7602
LeihaoChen <chenandrew683@gmail.com>
2023-02-22 16:19:58 -0500

    no timeout but wrong

--- mac/15651/hw4/dpq.py
+++ mac/15651/hw4/dpq.py
@@ -2,7 +2,6 @@ import math
 import sys
 
 
-
 class SegTree:
     def LeftChild(self, u: int):
         return 2 * u + 1
@@ -27,7 +26,7 @@ class SegTree:
             A[u] = self.reducer(lres, rres)
 
         self.n = len(a)
-        A = [(0, 0)] * (2 * self.n - 1)
+        A = [(0, 0, 0)] * (2 * self.n - 1)
         for i in range(self.n):
             A[i + self.n - 1] = a[i]
         build(0, 0, self.n)
@@ -62,34 +61,23 @@ class SegTree:
     #     return self.A[i + self.n - 1:j + self.n - 1]
 
 
-# test =[(3, 1),
-# (1, 1),
-# (10, 1),
-# (8, 1),
-# (4, 1),
-# (5, 1),
-# (9, 1),
-# (2, 1),]
-# def reducer(x, y):
-#     return (max(x[0], y[0]), x[1] + y[1])
-# st = SegTree(test, reducer=reducer)
-# st.assign(3, (11, 1))
-# print(st.rangeSum(0, 3))
-# print(st.rangeSum(0, 8))
-# print(st.rangeSum(4, 8))
-# print(st.rangeSum(2, 3))
-# print(st.rangeSum(1, 6))
-MOD = 92779
+MOD1 = 997
+# MOD2 = 97
 # MOD = 1000000007
 radix = 62
 
 
-def forward_hash(x, y):
-    # return hash(x+y)
-    modx, nx = x
-    mody, ny = y
+def barxy(x, y, ny):
+    return ((pow(radix, ny, MOD1) * x) % MOD1 + y) % MOD1
 
-    return ((pow(radix, ny, MOD) * modx) % MOD + mody) % MOD, nx + ny
+def hash(x, y):
+    # return hash(x+y)
+    modxf, modxr, nx = x
+    modyf, modyr, ny = y
+
+    return ((pow(radix, ny, MOD1) * modxf) % MOD1 + modyf) % MOD1, \
+           ((pow(radix, nx, MOD1) * modyr) % MOD1 + modxr) % MOD1, \
+           nx + ny
 
 
 APLHABET = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
@@ -98,18 +86,6 @@ d = {c: i for i, c in enumerate(APLHABET)}
 def c2i(c):
     return d[c]
 
-
-# test = [(c2i('b'), 1), (c2i('a'), 1), (c2i('n'), 1), (c2i('a'), 1), (c2i('n'), 1), (c2i('a'), 1)] + [(c2i(''), 0)] * 10
-# forward = SegTree(test, reducer=forward_hash)
-# reverse = SegTree(test[::-1], reducer=forward_hash)
-# print(forward.rangeSum(1, 6))
-# print(reverse.rangeSum(2, 7))
-# print(forward.rangeSum(0, 5))
-# print(reverse.rangeSum(11, 16))
-# forward.assign(0, (c2i('n'), 1))
-# reverse.assign(15, (c2i('n'), 1))
-# print(forward.rangeSum(0, 5))
-# print(reverse.rangeSum(11, 16))
 
 class DynamicPalindrome:
     def reverse_indicer(self, i):
@@ -120,38 +96,36 @@ class DynamicPalindrome:
         next_power_of_2 = pow(2, math.ceil(math.log2(x)))
         padding = next_power_of_2 - x
         self.n = next_power_of_2
-        input = [(c2i(c), 1) for c in input] + [(0, 0)] * padding
-        self.forward = SegTree(input, reducer=forward_hash)
-        self.reverse = SegTree(input[::-1], reducer=forward_hash)
+        input = [(c2i(c), c2i(c), 1) for c in input] + [(0, 0, 0)] * padding
+        self.forward = SegTree(input, reducer=hash)
+        # self.reverse = SegTree(input[::-1], reducer=hash)
 
     def modify(self, i, c):
-        self.forward.assign(i, (c2i(c), 1))
-        self.reverse.assign(self.reverse_indicer(i), (c2i(c), 1))
+        self.forward.assign(i, (c2i(c), c2i(c), 1))
+        # self.reverse.assign(self.reverse_indicer(i), (c2i(c), c2i(c), 1))
 
     def query(self, i, j):
         f = self.forward.rangeSum(i, j + 1)
-        r = self.reverse.rangeSum(self.reverse_indicer(j + 1) + 1, self.reverse_indicer(i) + 1)
-        return f == r
+        # r = self.reverse.rangeSum(self.reverse_indicer(j + 1) + 1, self.reverse_indicer(i) + 1)
+        return f[0] == f[1]
 
     def query2(self, i, j):
         f = self.forward.rangeSum(i, j + 1)
-        r = self.reverse.rangeSum(self.reverse_indicer(j + 1) + 1, self.reverse_indicer(i) + 1)
-        return f, r
+        # r = self.reverse.rangeSum(self.reverse_indicer(j + 1) + 1, self.reverse_indicer(i) + 1)
+        return f
 
     def pick(self, i, k, j):
+        if i == k == j:
+            return True
         if i == k:
-            return self.query(k+1, j)
+            return self.query(k + 1, j)
         elif j == k:
-            return self.query(i, k-1)
-        f1, r1 = self.query2(i, k-1)
-        f2, r2 = self.query2(k+1, j)
-        return forward_hash(f1, f2) == forward_hash(r2, r1)
-        # self.forward.assign(k, (c2i(''), 0))
-        # self.reverse.assign(self.reverse_indicer(k), (c2i(''), 0))
-        # retval = self.query(i, j)
-        # self.forward.assign(k, (c2i(self.raw[k]), 1))
-        # self.reverse.assign(self.reverse_indicer(k), (c2i(self.raw[k]), 1))
-        # return retval
+            return self.query(i, k - 1)
+        x, y, nleft = self.query2(i, k - 1)
+        s, t, nright = self.query2(k + 1, j)
+        xs = barxy(x, s, nright)
+        ty = barxy(t, y, nleft)
+        return xs == ty
 
     # def get_range(self, i, j):
     #     f = self.forward.get_range(i, j + 1)
@@ -185,10 +159,10 @@ def execute_test(file=sys.stdin):
 
 
 if __name__ == '__main__':
-    # execute_test()
+    execute_test()
 
-    for i in range(1, 16):
-        print('Test {}'.format(i))
-        with open('small-tests/{}.in'.format(str(i).zfill(2)), 'r') as f:
-            log = execute_test(f)
-        assert log == open('small-tests/{}.out'.format(str(i).zfill(2)), 'r').read().strip().split('\n')
+    # for i in range(1, 16):
+    #     print('Test {}'.format(i))
+    #     with open('small-tests/{}.in'.format(str(i).zfill(2)), 'r') as f:
+    #         log = execute_test(f)
+    #     assert log == open('small-tests/{}.out'.format(str(i).zfill(2)), 'r').read().strip().split('\n')
